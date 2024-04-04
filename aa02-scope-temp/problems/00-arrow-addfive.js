7839bd94527509fb8d4d5bd5602187247802a847
Cannicakee <stanfill11@gmail.com>
2024-04-03 22:41:20 -0600

    Solved Problem 0

--- aa02-scope-temp/problems/00-arrow-addfive.js
+++ aa02-scope-temp/problems/00-arrow-addfive.js
@@ -15,8 +15,14 @@ let result3 = addFive(37); // returns 42
 
 ***********************************************************************/
 
-// Your code here 
+const addFive = (num) => num + 5;
 
+let result1 = addFive(0); // returns 5
+console.log(result1);
+let result2 = addFive(10); // returns 15
+console.log(result2);
+let result3 = addFive(37); // returns 42
+console.log(result3);
 /**************DO NOT MODIFY ANYTHING UNDER THIS LINE*****************/
 try {
   module.exports = addFive;
