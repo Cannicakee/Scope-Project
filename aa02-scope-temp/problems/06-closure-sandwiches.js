b36616758f392ef2fd76e64daeb0f9f9fc7c57d8
Cannicakee <stanfill11@gmail.com>
2024-04-04 23:05:14 -0600

    Solved Problem 6

--- aa02-scope-temp/problems/06-closure-sandwiches.js
+++ aa02-scope-temp/problems/06-closure-sandwiches.js
@@ -1,7 +1,7 @@
 /***********************************************************************
 Let's practice writing closures by creating a function named `sandwichMaker()`.
 This function will return another function that will accept a string to add
-to the sandwich order (which will start off with a default "tomato" ingredient),
+to the sandwich order which will start off with a default "tomato" ingredient),
 separating each incoming ingredient with "and".
 
 Look below to see how this function is invoked:
@@ -13,12 +13,27 @@ sandwich("bread") // => "One sandwich with tomato and spinach and jelly and brea
 
 Another Sandwich:
 let sandwich2 = sandwichMaker(); // => returns a function
-sandwich2("pb") // => "One sandwich with tomato and pb"
+sandwich2("pb") // => "One console.log(sandwich with tomato and pb"
 
 ***********************************************************************/
+function sandwichMaker() {
+  let sandwichString = "One sandwich with tomato";
 
-// Your code here 
+  return function saySandwich(ingredient) {
+  sandwichString += " and "
+  sandwichString += ingredient
+  return sandwichString;
+  }
+}
 
+let sandwich = sandwichMaker(); // => returns a function
+
+console.log(sandwich("spinach")) // => "One sandwich with tomato and spinach"
+console.log(sandwich("jelly")) // => "One sandwich with tomato and spinach and jelly"
+console.log(sandwich("bread")) // => "One sandwich with tomato and spinach and jelly and bread"
+
+let sandwich2 = sandwichMaker(); // => returns a function
+console.log(sandwich2("pb")) // => "One sandwich with tomato and pb"
 /**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
 
 try {
