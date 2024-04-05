f62f876047fef079eb34839c1d65431987147814
Kayleen Sandoval <kayleen.sandoval02@gmail.com>
2024-04-04 22:08:31 -0500

    Solved problem 4

--- aa02-scope-temp/problems/04-arrow-average-value.js
+++ aa02-scope-temp/problems/04-arrow-average-value.js
@@ -10,7 +10,24 @@ arrowAvgValue([2, 3, 7]); // => 4
 arrowAvgValue([100, 60, 64]); // => 74.66666666666667
 ***********************************************************************/
 
-// Your code here 
+const arrowAvgValue = (array) => {
+  let sum = 0
+  array.forEach((el) => {
+    sum += el
+
+
+
+  })
+
+return sum/array.length
+
+
+}
+
+
+console.log(arrowAvgValue([10, 20])); // => 15
+console.log(arrowAvgValue([2, 3, 7])); // => 4
+console.log(arrowAvgValue([100, 60, 64])); // => 74.66666666666667
 
 /**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
 
