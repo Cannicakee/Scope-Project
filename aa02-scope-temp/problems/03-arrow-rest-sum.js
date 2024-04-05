dcf607916bc52ac77abf3a38ef5279f1daa29ac0
Kayleen Sandoval <kayleen.sandoval02@gmail.com>
2024-04-04 21:51:17 -0500

    Solved problem 3

--- aa02-scope-temp/problems/03-arrow-rest-sum.js
+++ aa02-scope-temp/problems/03-arrow-rest-sum.js
@@ -10,8 +10,20 @@ arrowRestSum(3,5,6); // => 14
 arrowRestSum(1, 2, 3, 4, 5, 6, 7, 8, 9); // => 45
 arrowRestSum(0); // => 0
 ***********************************************************************/
+const arrowRestSum = (...num) => {
+  let sum = 0
+  num.forEach((el) => {
+  sum += el
+  })
+  return sum 
 
-// Your code here 
+
+}
+
+
+console.log(arrowRestSum(3,5,6)); // => 14
+console.log(arrowRestSum(1, 2, 3, 4, 5, 6, 7, 8, 9)); // => 45
+console.log(arrowRestSum(0)); // => 0
 
 /**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
 
